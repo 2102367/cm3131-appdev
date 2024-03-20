0ac090075446d55250b21d15b8583e7961ae7b66
duhrnise <146590344+duhrnise@users.noreply.github.com>
2024-03-20 20:09:48 +0000

    put all my files in folder

---
+++ app.js
@@ -0,0 +1,110 @@
+const imageDisplay = document.getElementById("img-display");
+const productTitle = document.getElementById("title-name");
+const searchButton = document.getElementById("btn-search");
+const outputList = document.getElementById("list-output");
+
+//old search that was only text input
+// const searchtTag = document.getElementById("input-search-brand");
+// const searchProduct = document.getElementById("input-search-product");
+
+const selectProduct = document.getElementById("select-product");
+const selectTags = document.getElementById("select-tag");
+
+const wishlistBtn = document.getElementById("wishlist-btn");
+
+let currentListArray = {};
+let currentListItem = {};
+let wishlistArray = [];
+
+searchButton.addEventListener("click", getProducts);
+
+// wishlistBtn.addEventListener("click", getCurrentProduct);
+
+function getProducts(){
+    // let searchInputTxt = document.getElementById("input-search").value;
+
+    fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_tags=${selectTags.value}&product_type=${selectProduct.value}`).then(getJson).then(updateDisplay).catch(reportError);
+}
+
+function getJson(aResponse){
+    // console.log(aResponse);
+
+    const theResponse = aResponse.json();
+    console.log(theResponse);
+
+    return theResponse;
+}
+
+function updateDisplay(jsonObj){
+
+    let currentListArray = jsonObj;
+    console.log(currentListArray);
+
+    outputList.innerHTML = "";
+
+    for(let i = 0; i < currentListArray.length; i++){
+       
+        let brandName = currentListArray[i].brand;
+        let productName = currentListArray[i].name;
+        let productImage = currentListArray[i].image_link;
+        let productTags = currentListArray[i].tag_list;
+        let productDesc = currentListArray[i].description;
+        let productRating = currentListArray[i].rating;
+        // console.log(productTags);        
+
+        let card = document.createElement("ion-card");
+
+        let cardContent = document.createElement("ion-card-content");
+
+        cardContent.innerHTML = `
+        <ion-card>
+            <ion-img expand="full"src="${productImage}"></ion-img>
+            <ion-card-header>
+                <ion-card-title>
+                    Name: ${productName}
+                </ion-card-title>
+                <ion-card-subtitle>
+                    Brand: ${brandName}
+                </ion-card-subtitle>
+            </ion-card-header>
+
+
+            <ion-card-content>
+
+                // add rating here maybe
+
+                <p>${productDesc}</p>
+                <br>
+                <ion-button expand="block" id="wishlist-btn" onclick='saveItem("${productName}", "${brandName}", "${productImage}")'>Add to list</ion-button>
+                <ion-button expand="block">Button</ion-button>
+                
+            </ion-card-content>
+
+        <ion-card>
+        `;
+
+        //den pls dont forget to add da link to buttons (currentListArray[].website_link)
+        //add rating
+        outputList.appendChild(cardContent);
+    }
+}
+
+function saveItem(name, brand, image_link, description){
+    //removed description as there are problems with commas and parentheses in description
+
+    let item = {
+        productName: name ,
+        brandName: brand ,
+        imageLink: image_link,
+        // description: description,
+    };
+
+    wishlistArray.push(item);
+    console.log(wishlistArray);
+}
+
+
+function reportError(anError){
+    console.log(anError);
+}
+
---
+++ random.js
@@ -0,0 +1,56 @@
+//this is really old code for making the updateDisplay html, found a shorter way        
+        
+        
+        // let image = document.createElement("img");
+        // image.src = productImage;
+        
+        // let cardHeader = document.createElement("ion-card-header");
+
+        // let productTitle = document.createElement("ion-card-title");
+        // let brandTitle = document.createElement("ion-card-subtitle");
+        // let tagsList = document.createElement("ion-list");
+        // let tags = document.createElement("ion-item");
+        // let wishlistBtn = document.createElement("ion-button");
+        // let otherBtn = document.createElement("ion-button");
+
+        // let description = document.createElement("p");
+
+        
+
+        // productTitle.textContent = productName;
+        // brandTitle.textContent = brandName;
+        // description.textContent = productDesc;
+
+        // card.appendChild(cardContent);
+        // card.appendChild(image);
+
+        // card.appendChild(cardHeader);
+
+
+        // cardHeader.appendChild(productTitle);
+        // cardHeader.appendChild(brandTitle);
+        // card.appendChild(wishlistBtn);
+        // card.appendChild(otherBtn);
+
+        // cardContent.appendChild(description);
+
+
+        //not important code
+        // cardContent.appendChild(tagsList);
+
+        // for(let i = 0; i < productTags.length; i++){
+        //     tags.textContent = productTags[i];
+        //     tagsList.appendChild(tags);
+
+        //     // console.log(productTags[i]);
+        // }
+
+        // wishlistBtn.id = "wishlist-btn";
+        // wishlistBtn.textContent = "wishlist";
+        // wishlistBtn.fill = "clear";
+
+        // otherBtn.id = "other-btn";
+        // otherBtn.textContent = "wishlist";
+        // otherBtn.fill = "clear";
+
+        // outputList.appendChild(card);
